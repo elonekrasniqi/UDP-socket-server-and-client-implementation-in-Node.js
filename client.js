a301f0d609773bf94256a32cb8c04945329227d8
elionmehaj <mehajelion@gmail.com>
2024-11-07 22:11:52 +0100

    .

--- client.js
+++ client.js
@@ -1,10 +1,9 @@
-touch client.js
-nano client.js
-
 const dgram = require('dgram');
 const readline = require('readline');
 
 const SERVER_IP = '172.20.10.2';
 const SERVER_PORT = 3500;
 
-
+const client = dgram.createSocket('udp4');
+let isConnected = false;
+let inChatMode = false;
